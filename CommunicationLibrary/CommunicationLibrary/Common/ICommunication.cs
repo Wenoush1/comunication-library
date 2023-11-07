d87b6df8b7c3144f800e2922938c9d8cfadbb7fc
Wenoush1 <vaclavturek55@gmail.com>
2023-11-07 20:55:38 +0100

    Created project and implemented communication interface

---
+++ CommunicationLibrary/CommunicationLibrary/Common/ICommunication.cs
@@ -0,0 +1,16 @@
+﻿namespace CommunicationLibrary.Common;
+
+/// <summary>
+/// Defines the contract for sending messages to a recipient.
+/// </summary>
+public interface ICommunication
+{
+    /// <summary>
+    /// Sends a message to the specified recipient with the given subject and message content.
+    /// </summary>
+    /// <param name="recipient">The recipient's address or identifier.</param>
+    /// <param name="subject">The subject or title of the message.</param>
+    /// <param name="message">The content of the message to be sent.</param>
+    void Send(string recipient, string subject, string message);
+
+}
